3cbf9c324ae471f305e922ccdd356356887153eb
Toskosz <thiago.tokarski2001@gmail.com>
2023-02-19 04:05:11 -0300

    feat: Product model

---
+++ models/product.py
@@ -0,0 +1,14 @@
+
+
+Class Product:
+
+    def __init__(self) -> None:
+        self.internal_code = ""
+        self.barcode = [""]
+        self.name = ""
+        self.price = 0.00
+        self.promo_price = 0.00
+        self.promo_end_at = ""
+        self.stock = 0
+        self.visible = False
+
