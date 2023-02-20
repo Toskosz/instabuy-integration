e2b985e8474032ed236bb9d8b7f6c31c52758133
thiago <thiago.tokarski2001@gmail.com>
2023-02-20 15:51:48 -0300

    fix: update batch size

--- src/integration.py
+++ src/integration.py
@@ -4,6 +4,8 @@ class Integration:
 
     def __init__(self, endpoint: str) -> None:
         self.endpoint = endpoint
+        self.file = None
+        self.reader = None
 
     def load(self):
         pass
@@ -13,11 +15,19 @@ class Integration:
 
     def update(self, file_path="", batch_size=None) -> None:
 
-        while (self.load(file_path)):
-            payload = self.payload("put")
-            response = API.put(endpoint=self.endpoint, payload=payload)
+        batch_count = 0
+        while(self.load(file_path)):
+            batch_count += 1
+            if batch_count >= batch_size:
+                payload = self.payload("put")
+                response = API.put(endpoint=self.endpoint, payload=self.payload("put"))
+                batch_count = 0
         
-        return
+        # The last batch's size may be less then batch_size
+        # TODO: If last one is empty
+        response = API.put(endpoint=self.endpoint, payload=self.payload("put"))
+
+
 
    def _file_init(self, file_path: str) -> None:
         self.file = open(file_path, 'r')
@@ -25,5 +35,6 @@ class Integration:
 
     def _file_terminate(self) -> None:
         self.file.close()
+        self.file = None
         self.reader = None
 
