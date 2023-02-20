e27fe0e591969d4a20764bf3b287b02688efa533
thiago <thiago.tokarski2001@gmail.com>
2023-02-19 22:13:47 -0300

    feat: add api retry mech and put method

--- src/api.py
+++ src/api.py
@@ -1,7 +1,23 @@
-
+import requests
+import json
 
 class API:
 
     host: str
     api_key: str
+    api_session: requests.Session
 
+    @staticmethod
+    def setup(self):
+        self.api_session = requests.Session()
+        # Retry mechanism
+        retries = Retry(total=3, backoff_factor=1, status_forcelist=[500])
+        self.api_session.mount('http://', HTTPAdapter(max_retries=retries))
+
+    @staticmethod
+    def put(endpoint:str, payload: dict) -> dict:
+        
+        response = requests.put(self.host+endpoint,
+                json.dumps(payload), headers=self.headers)
+
+        return json.loads(response.content)['data']
