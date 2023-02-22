147a743a2a77803a5a4fac2c698118d8740a6827
thiago <thiago.tokarski2001@gmail.com>
2023-02-22 03:27:15 -0300

    feat: api tests

--- tests/api_test.py
+++ tests/api_test.py
@@ -5,26 +5,23 @@ class TestAPI(unittest.TestCase):
     maxDiff = None
 
     def test_setup(self):
-        API.setup("test")
-
         desired_headers = {
                 "api-key": "123",
                 "Content-Type": "application/json"
         }
-
-        self.assertEqual(API.host, "https://httpbin.org/")
-        self.assertEqual(API.api_session.headers["api-key"], desired_headers["api-key"])
-        self.assertEqual(API.api_session.headers["Content-Type"], desired_headers["Content-Type"])
+        with API.conn("test"):
+            self.assertEqual(API.host, "https://httpbin.org/")
+            self.assertEqual(API.api_session.headers["api-key"], desired_headers["api-key"])
+            self.assertEqual(API.api_session.headers["Content-Type"], desired_headers["Content-Type"])
 
     def test_put(self):
         data = {
             "key": "value",
         }
 
-        API.setup("test")
-        response = API.put("put", data)
-        response = API.put("put", data)
-        self.assertEqual(response["url"], "https://httpbin.org/put")
+        with API.conn("test"):
+            response = API.put("put", data)
+            self.assertEqual(response["url"], "https://httpbin.org/put")
 
 if __name__ == '__main__':
     unittest.main()
